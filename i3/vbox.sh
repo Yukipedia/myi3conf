770114b95c41b6e4e33146b515f67cf4944352c7
Yukipedia <pynds68@outlook.com>
2020-07-17 08:46:06 +0800

    update

--- i3/vbox.sh
+++ i3/vbox.sh
@@ -1,7 +1,5 @@
 VBoxManage startvm Windows --type headless 2>/dev/null
 
-sleep 30
-
 VBoxHeadlessPID=$(ps -A | grep VBoxHeadless)
 VBoxHeadlessPID=$(echo $VBoxHeadlessPID | cut -d'?' -f1)
 echo "VBoxHeadlessPID: ${VBoxHeadlessPID}"
@@ -10,7 +8,7 @@ kill -9 $VBoxHeadlessPID
 sleep 2
 VBoxManage startvm Windows --type headless
 
-$HOME/.rdp/work.sh
+$HOME/.rdp/company_local.sh
 
 while [ $? -ne 0 ]
 do
@@ -18,5 +16,5 @@ do
 	if [ -z "$VBoxHeadlessPID" ]; then
 		break
 	fi
-	$HOME/.rdp/work.sh
+	$HOME/.rdp/company_local.sh
 done
